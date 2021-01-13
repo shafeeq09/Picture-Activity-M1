b37f096f798ed41cf9eb0d8db28a65d9da4abc2b
anikakanigeri <77178792+anikakanigeri@users.noreply.github.com>
2021-01-13 12:32:22 -0800

    Create monkeyCode.c
    This is my monkey for the character activity!

---
+++ monkeyCode.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    
+    printf("           _______________\n");
+    printf("          ( ooh ooh ah ah  )\n");
+    printf("           _  ____________/\n");
+    printf("            )/\n");
+    printf("   __,,,__  \n");
+    printf(" (d .   . b)\n");
+    printf("   ( (Y) )\n");
+    printf("   <|   |>\n");
+    printf("   (^)_(^)_/\n");
+   
+    
+    return 0;
+    
+}
