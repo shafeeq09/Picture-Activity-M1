47fd26ec080d3a681ed19cd33e7676374d731a65
HaranSS <77175709+HaranSS@users.noreply.github.com>
2021-01-13 15:58:04 -0800

    Create Robot.C

---
+++ Robot.C
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main (void){
+printf("        ___________\n ");
+printf("      |  ()   ()  |\n");
+printf("       |           |\n");
+printf("       |  +++++++  |\n");
+printf("       |___________|\n");
+printf("      /  / |    |\\  \\ \n ");
+printf("    /  /  |    | \\  \\ \n");
+printf("    /  /   |    |  \\  \\ \n");
+printf("   |__|    |    |   |__|\n");
+printf("   (  )    |    |   (  )\n");
+printf("           |    |\n");
+printf("           |    |\n");
+printf("          /  /\\  \\ \n");
+printf("         /  /  \\  \\ \n");
+printf("        /  /    \\  \\ \n");
+printf("       /  /      \\  \\  \n");
+printf("     _/  /        \\  \\_\n");
+printf("    |____|         |____|\n");
+
+
+
+return 0;
+}
