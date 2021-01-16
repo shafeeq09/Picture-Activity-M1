47732ae0564dc0f7b344e98b0871c7f8f909026f
shafeeq09 <77204906+shafeeq09@users.noreply.github.com>
2021-01-15 21:18:42 -0800

    Create cheeto.c

---
+++ cheeto.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("          |\\__/|\n");
+    printf("          (_ ^-^)\n");
+    printf(" _         )    (\n");
+    printf("( (       /      \\    ( ( (\n");
+    printf("  (    )  | |  | |  ) ) )\n");
+    printf("  '----'  '--' '--' >+++ >\n");
+    return 0;
+}
+
